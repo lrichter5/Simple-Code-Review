bc3558ffd98b148509fadef920cf497bb243e38c
lrichter5 <len.rich.777@gmail.com>
2018-01-09 09:46:50 -0700

    Created Visual Studio Console App

---
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeReview
+{
+    class Salutation
+    {
+        private string _Greeting;
+        private string _Farewell;
+
+        public Salutation(string greetingMessage, string farewellMessage)
+        {
+            _Greeting = greetingMessage;
+            _Farewell = farewellMessage;
+        }
+
+        public string Greet()
+        {
+            return _Greeting;
+        }
+        public string SayFareWell()
+        {
+            return _Farewell;
+        }
+    }
+}
